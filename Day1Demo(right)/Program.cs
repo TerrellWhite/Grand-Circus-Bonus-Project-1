71dca9b9506dd4ad8d053a2be5db3bf905bc4531
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-17 14:17:53 -0400

    First Commit

---
+++ Day1Demo(right)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Demo_right_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this program will read two numbers, then show the sum of the numbers
+
+            //input
+            int Input1;
+            int Input2;
+
+            Console.WriteLine("Please enter a number ");
+            Input1 = int.Parse(Console.ReadLine()); //int.parse converts str in to int
+            Console.WriteLine("Please enter a number ");
+            Input2 = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32 does the same
+            //processing
+            int Result = Input1 + Input2;
+            //output
+            Console.WriteLine($"The result of adding {Input1} and {Input2} is {Result}"); //$allows you to embedd code in string        
+        }
+    }
+}
